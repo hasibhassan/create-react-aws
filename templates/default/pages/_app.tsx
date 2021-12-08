b39f85316780b61ff40d0e64509fc53fc3df1aed
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-12-07 22:03:57 -0600

    refactor: update default template

--- templates/default/pages/_app.tsx
+++ templates/default/pages/_app.tsx
@@ -1,8 +1,13 @@
 import 'tailwindcss/tailwind.css'
+import { RecoilRoot } from 'recoil'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
